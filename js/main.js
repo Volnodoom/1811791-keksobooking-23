c0395f73c73e976e3313a8a51f13c60e3d268183
Alexander <Bdiyoz@mail.ru>
2021-06-01 12:41:39 +0400

    Correction of the second function: corrected restrictions (now work properly), remove console call from the code

--- js/main.js
+++ js/main.js
@@ -2,32 +2,29 @@
 const pickUpRandomNumber = function (leftLimit,rightLimit) {
   leftLimit = Math.ceil (leftLimit);
   rightLimit = Math.floor (rightLimit);
-  const randomNumber = Math.floor (Math.random()*(rightLimit-leftLimit+1))+leftLimit;
-  if (leftLimit >=0 && rightLimit>=0 && rightLimit>=leftLimit) {
+  const randomNumber = Math.floor(Math.random() * (rightLimit - leftLimit + 1)) + leftLimit;
+  if (leftLimit >= 0 && rightLimit >= 0 && rightLimit >= leftLimit) {
     return randomNumber;
-  } else if (leftLimit >=0 && rightLimit>=0 && rightLimit<leftLimit) {
-    const numberSwitch = leftLimit;
-    leftLimit = rightLimit;
-    rightLimit = numberSwitch;
-    return randomNumber;
+  } else if (leftLimit >=0 && rightLimit >= 0 && rightLimit < leftLimit) {
+    return ('First value (limit) should be greater then second value (limit).');
   }
-  return 'The numbers shoud be greater or equalt to ziro! Please, reinter the interval';
+  return 'The numbers should be greater or equal to zero! Please, reinter the interval';
 };
 pickUpRandomNumber (16,54);
 
 
-const giveKeksobookingRandomNumber = function (leftLimit,rightLimit, numberOfSymbols) {
+const giveKeksobookingRandomNumber = function (leftLimit,rightLimit,numberOfSymbols) {
   leftLimit = (Math.round(leftLimit * Math.pow(10,numberOfSymbols)));
   rightLimit = (Math.round(rightLimit * Math.pow(10,numberOfSymbols)));
-  const randomness = Math.random()*(rightLimit-leftLimit+1);
-  const randomNumber = (Math.floor (randomness)+leftLimit)/Math.pow(10,numberOfSymbols); // The function  for random non-integer number have been taken from the MDN website with some small adjustments: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  const restrictions = leftLimit >=0 && rightLimit>=0 && (typeof leftLimit === typeof rightLimit) && (typeof leftLimit=== 'number');
-  if ( restrictions && rightLimit>=leftLimit ) {
+  const randomness = Math.random() * (rightLimit - leftLimit + 1);
+  const randomNumber = (Math.floor (randomness) + leftLimit)/Math.pow(10,numberOfSymbols); // The function  for random non-integer number have been taken from the MDN website with some small adjustments: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  const restriction = (leftLimit >= 0 && rightLimit >= 0 && (typeof leftLimit === typeof rightLimit) && (typeof leftLimit === 'number'));
+  if ( restriction && rightLimit >= leftLimit ) {
     return randomNumber;
-  } else if (restrictions && rightLimit<leftLimit) {
+  } else if (restriction && rightLimit < leftLimit) {
     return ('First value (limit) should be greater then second value (limit).');
   }
-  return (` You need to write numbers that should be greater or equal to zero! Please, reinter the interval. \n Remind you to text how many symbols after comma you want to see, current number is ${  numberOfSymbols}`);
+  return (` You need to write numbers that should be greater or equal to zero! Please, reinter the interval. \n Remind you to put number of symbols you want to see after comma, current number is ${  numberOfSymbols}`);
 };
 giveKeksobookingRandomNumber (1000.2654165,875.147923,2);
-console.log(giveKeksobookingRandomNumber (-1000.2654165,875.147923,2));
+
