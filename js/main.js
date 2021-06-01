eb0bc689c0a5716b7a329a7afff4f15a15293b52
Alexander <Bdiyoz@mail.ru>
2021-06-01 12:37:29 +0400

    Correction of the second function: changed else if line, corrected formation and syntaxis of lines

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
-//The function bellow for random integer number have been taken from the MDN webside: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const PICK_UP_RANDOM_NUMBER = function (leftLimit,rightLimit) {
+//The function bellow for random integer number have been taken from the MDN website: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const pickUpRandomNumber = function (leftLimit,rightLimit) {
   leftLimit = Math.ceil (leftLimit);
   rightLimit = Math.floor (rightLimit);
-  const RANDOM_NUMBE = Math.floor (Math.random()*(rightLimit-leftLimit+1))+leftLimit;
+  const randomNumber = Math.floor (Math.random()*(rightLimit-leftLimit+1))+leftLimit;
   if (leftLimit >=0 && rightLimit>=0 && rightLimit>=leftLimit) {
-    return RANDOM_NUMBE;
+    return randomNumber;
   } else if (leftLimit >=0 && rightLimit>=0 && rightLimit<leftLimit) {
-    const NUMBER_SWITCH = leftLimit;
+    const numberSwitch = leftLimit;
     leftLimit = rightLimit;
-    rightLimit = NUMBER_SWITCH;
-    return RANDOM_NUMBE;
+    rightLimit = numberSwitch;
+    return randomNumber;
   }
   return 'The numbers shoud be greater or equalt to ziro! Please, reinter the interval';
 };
-PICK_UP_RANDOM_NUMBER (16,54);
+pickUpRandomNumber (16,54);
 
 
-const GIVE_KEKSOBOOKING_RANDOM_NUMBER = function (leftLimit,rightLimit, numberOfSymbols) {
-  leftLimit = (Math.round(leftLimit * Math.pow(10,numberOfSymbols))); //rounding according to the number of simbols after comma
+const giveKeksobookingRandomNumber = function (leftLimit,rightLimit, numberOfSymbols) {
+  leftLimit = (Math.round(leftLimit * Math.pow(10,numberOfSymbols)));
   rightLimit = (Math.round(rightLimit * Math.pow(10,numberOfSymbols)));
-  const RANDOMNESS = Math.random()*(rightLimit-leftLimit+1);
-  const RANDOM_NUMBE = (Math.floor (RANDOMNESS)+leftLimit)/Math.pow(10,numberOfSymbols); // The function  for random non-integer number have been taken from the MDN webside with some small adjustments: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  const RESTRICTIONS = leftLimit >=0 && rightLimit>=0 && (typeof leftLimit === typeof rightLimit) && (typeof leftLimit=== 'number');
-  if ( RESTRICTIONS && rightLimit>=leftLimit ) {
-    return RANDOM_NUMBE;
-  } else if (RESTRICTIONS && rightLimit<leftLimit) {
-    const NUMBER_SWITCH = leftLimit;
-    leftLimit = rightLimit;
-    rightLimit = NUMBER_SWITCH;
-    return RANDOM_NUMBE;
+  const randomness = Math.random()*(rightLimit-leftLimit+1);
+  const randomNumber = (Math.floor (randomness)+leftLimit)/Math.pow(10,numberOfSymbols); // The function  for random non-integer number have been taken from the MDN website with some small adjustments: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  const restrictions = leftLimit >=0 && rightLimit>=0 && (typeof leftLimit === typeof rightLimit) && (typeof leftLimit=== 'number');
+  if ( restrictions && rightLimit>=leftLimit ) {
+    return randomNumber;
+  } else if (restrictions && rightLimit<leftLimit) {
+    return ('First value (limit) should be greater then second value (limit).');
   }
-  return 'You need to write numbers that shoud be greater or equalt to ziro! Please, reinter the interval';
+  return (` You need to write numbers that should be greater or equal to zero! Please, reinter the interval. \n Remind you to text how many symbols after comma you want to see, current number is ${  numberOfSymbols}`);
 };
-GIVE_KEKSOBOOKING_RANDOM_NUMBER (10.2654165,875.147923,2);
-
+giveKeksobookingRandomNumber (1000.2654165,875.147923,2);
+console.log(giveKeksobookingRandomNumber (-1000.2654165,875.147923,2));
