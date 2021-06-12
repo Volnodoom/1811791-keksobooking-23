26f8cd6060774241109f7e73ff56dbe1ae4f3b14
Alexander <Bdiyoz@mail.ru>
2021-06-12 17:00:02 +0400

    Home work module4-task1: review have been madea, code corrected

--- js/main.js
+++ js/main.js
@@ -1,87 +1,54 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-function getRandomPositiveInteger (valueA, valueB) {
-  const lower = Math.ceil(Math.min(Math.abs(valueA), Math.abs(valueB)));
-  const upper = Math.floor(Math.max(Math.abs(valueA), Math.abs(valueB)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
+const getRandomPositiveInteger = function (valueA, valueB) {
+  const lower = Math.ceil (Math.min (Math.abs (valueA), Math.abs (valueB)));
+  const upper = Math.floor (Math.max (Math.abs (valueA), Math.abs (valueB)));
+  const result = Math.random () * (upper - lower + 1) + lower;
+  return Math.floor (result);
+};
 
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-function getRandomPositiveFloat (valueA, valueB, digits = 1) {
-  const lower = Math.min(Math.abs(valueA), Math.abs(valueB));
-  const upper = Math.max(Math.abs(valueA), Math.abs(valueB));
-  const result = Math.random() * (upper - lower) + lower;
-  return result.toFixed(digits);
-}
-getRandomPositiveFloat (0.4456443,874.78526234,5);
+const getRandomPositiveFloat = function (valueA, valueB, digits = 1) {
+  const lower = Math.min (Math.abs(valueA), Math.abs(valueB));
+  const upper = Math.max (Math.abs(valueA), Math.abs(valueB));
+  const result = Math.random () * (upper - lower) + lower;
+  return result.toFixed (digits);
+};
 
-/*
-goal is create massive with 10 generated JS-OBJECTS
+const getRandomItemNoRepeat = function (arrayData) {
+  const randomIndex = getRandomPositiveInteger (0, arrayData.length - 1);
+  const randomDataFromMassive = arrayData [randomIndex];
+  arrayData.splice (randomIndex, 1);
+  return randomDataFromMassive;
+};
 
-author, объект — описывает автора. Содержит одно поле:
-avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 8 с ведущим нулём.
-Например, 01, 02 и т. д. Адреса изображений не повторяются.
-
-offer, объект — содержит информацию об объявлении. Состоит из полей:
-title, строка — заголовок предложения. Придумайте самостоятельно.
-
-address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по
-маске {{location.x}}, {{location.y}}.
-
-price, число — стоимость. Случайное целое положительное число.
-type, строка — одно из пяти фиксированных значений: palace, flat, house, bungalow или hotel.
-rooms, число — количество комнат. Случайное целое положительное число.
-guests, число — количество гостей, которое можно разместить. Случайное целое положительное число.
-checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-description, строка — описание помещения. Придумайте самостоятельно.
-photos, массив строк — массив случайной длины из значений: https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg, https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.
-
-
-location, объект — местоположение в виде географических координат. Состоит из двух полей:
-lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
-lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
-*/
-
+const getRandomItemRepeat =  (arrayData) =>  arrayData[getRandomPositiveInteger (0, arrayData.length - 1)];
 
 const makeMassiveForAvatarka = function(numberA = 1, numberB = 8) {
   numberA = Math.abs(numberA);
   numberB = Math.abs(numberB);
   if ((numberB === 0) || (numberA === 0)) {
     return 'Please, place the Integer number greater than 0';}
-  const uslessMassive = new Array (numberB-numberA + 1).fill('');
-  for (let i = 0; i<uslessMassive.length; i++) {
-    uslessMassive[i] = numberA + i;
+  const massive = new Array (numberB-numberA + 1).fill('');
+  for (let i = 0; i<massive.length; i++) {
+    massive[i] = numberA + i;
   }
-  return uslessMassive;
-};
-const getRandomItemNoRepeat = function (arrayData = makeMassiveForAvatarka()) {
-  const randomIndexForAvatarka = getRandomPositiveInteger (0,arrayData.length-1);
-  const randomNumberFromMassive = arrayData [randomIndexForAvatarka];
-  arrayData.splice (randomIndexForAvatarka,1);
-  return randomNumberFromMassive;
+  return massive;
 };
 
 const MoscowX = 55.7558;
 const MoscowY = 37.6173;
 
-const typeOfAppartments = [
+const typeOfApartment = [
   'palace',
   'flat',
   'house',
   'bungalow',
   'hotel',
 ];
-
-const getRandomItemRepeat = function (arrayData) {
-  const randomIndex = getRandomPositiveInteger (0, arrayData.length - 1);
-  return arrayData[randomIndex];
-};
 
 const checkInTime = [
   '12:00',
@@ -91,7 +58,7 @@ const checkInTime = [
 
 const checkOutTime = checkInTime;
 
-const FeaturesOfAppartment =[
+const FeaturesOfApartment =[
   'wifi',
   'dishwasher',
   'parking',
@@ -105,35 +72,31 @@ const photosData = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-console.log (getRandomItemNoRepeat(2,5));
-
-
-
-/*
-
-*/
-
-
 
 const author = {
-  avatar:`/img/avatars/user0${getRandomItemNoRepeat()}.png`,
+  avatar:`/img/avatars/user0${getRandomItemNoRepeat(makeMassiveForAvatarka())}.png`,
 };
 const offer = {
   title: 'Доступные предложения',
   address: [MoscowX, MoscowY],
   price: getRandomPositiveInteger(500,20000),
-  type: getRandomItemRepeat(typeOfAppartments),
+  type: getRandomItemRepeat(typeOfApartment),
   rooms: getRandomPositiveInteger(1,5),
   guests: getRandomPositiveInteger(1,10),
   checkin: getRandomItemRepeat(checkInTime),
   checkout: getRandomItemRepeat(checkOutTime),
-  features: getRandomItemNoRepeat(FeaturesOfAppartment),
-  description: 'You are checking one of the greatest appartments on our market /n you could enjoy pleasure of your life here /n with appropriate badget and level of comfort',
+  features: getRandomItemNoRepeat(FeaturesOfApartment),
+  description: 'You are checking one of the greatest apartment on our market \n you could enjoy pleasure of your life here \n with appropriate budget and level of comfort',
   photos: getRandomItemRepeat(photosData),
 };
 
+const location = {
+  lat: getRandomPositiveFloat(35.65000, 35.70000,5),
+  lng: getRandomPositiveFloat (139.70000,139.80000,5),
+};
 
 const temptData = [
   author,
   offer,
+  location,
 ];
