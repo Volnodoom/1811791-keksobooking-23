a0966c4f9a0ac85187349ea6512df599554a5d9d
Alexander <Bdiyoz@mail.ru>
2021-06-18 22:06:19 +0400

    Corrected point of inter (main.js) and added corrected file location-lat-lng

--- js/main.js
+++ js/main.js
@@ -1,100 +1,14 @@
-const NUMBER_OF_AVATARS = 10;
-const NUMBER_WITHOUT_ZERO = 9;
-const TITLE = 'Available offers';
-const DESCRIPTION = 'You are checking one of the greatest apartment on our market \n you could enjoy pleasure of your life here \n with appropriate budget and level of comfort';
-const TYPE_OF_APARTMENT = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-  'hotel',
-];
-const TIMES = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-const FEATURES_OF_APARTMENT =[
-  'WiFi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-const PHOTOS_DATA = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
-
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-
-const getRandomPositiveInteger = function (valueA, valueB) {
-  const lower = Math.ceil (Math.min (Math.abs (valueA), Math.abs (valueB)));
-  const upper = Math.floor (Math.max (Math.abs (valueA), Math.abs (valueB)));
-  const result = Math.random () * (upper - lower + 1) + lower;
-  return Math.floor (result);
-};
-
-// Функция взята из интернета и доработана
-// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
-
-const getRandomPositiveFloat = function (valueA, valueB, digits = 1) {
-  const lower = Math.min (Math.abs(valueA), Math.abs(valueB));
-  const upper = Math.max (Math.abs(valueA), Math.abs(valueB));
-  const result = Math.random () * (upper - lower) + lower;
-  return result.toFixed (digits);
-};
-
-let start = 1;
-const refreshEmptyArray = function (array,arrayLength) {
-  while (start <= arrayLength) {
-    array.push(start++);
-  }
-};
-
-const arrayWithIncreasingNumbers = [];
-refreshEmptyArray(arrayWithIncreasingNumbers,NUMBER_OF_AVATARS);
-
-const getRandomItemNoRepeat = function (arrayData) {
-  const randomIndex = getRandomPositiveInteger (0, arrayData.length - 1);
-  const randomDataFromArray = arrayData [randomIndex];
-  arrayData.splice (randomIndex, 1);
-  return randomDataFromArray;
-};
-
-const getRandomItemRepeat = function (arrayData) {
-  const randomIndex = getRandomPositiveInteger (0, arrayData.length - 1);
-  return arrayData[randomIndex];
-};
-
-const getRandomLength = function (array) {
-  let randomNumber1 = getRandomPositiveInteger (0,(array.length - 1));
-  let randomNumber2 = getRandomPositiveInteger (1,(array.length));
-  while (randomNumber1 === randomNumber2) {
-    randomNumber1 = getRandomPositiveInteger (0,(array.length - 1));
-    randomNumber2 = getRandomPositiveInteger (1,(array.length));
-  }
-  const min = Math.min (randomNumber1,randomNumber2);
-  const max = Math.max (randomNumber1, randomNumber2);
-  return array.slice(min,max);
-};
-
-const getRandomNumberUnderCondition = function (randomNumber){
-  if (randomNumber > NUMBER_WITHOUT_ZERO) {
-    return `img/avatars/user${randomNumber}.png`;
-  } return `img/avatars/user0${randomNumber}.png`;
-};
+import {TITLE, TYPE_OF_APARTMENT, TIMES, FEATURES_OF_APARTMENT, DESCRIPTION, PHOTOS_DATA} from './modulesForKeksBooking/library.js';
+import {getRandomAvatar, getRandomPositiveInteger, getRandomItemRepeat, getRandomLength} from './modulesForKeksBooking/get-random.js';
+import {LAT, LNG} from './modulesForKeksBooking/location-lat-lng.js';
 
 const creatAvailableApartment = function () {
-  const LAT = getRandomPositiveFloat (35.65000, 35.70000,5);
-  const LNG = getRandomPositiveFloat (139.70000,139.80000,5);
+  LAT;
+  LNG;
 
   return {
     author : {
-      avatar:getRandomNumberUnderCondition(getRandomItemNoRepeat(arrayWithIncreasingNumbers)),
+      avatar:getRandomAvatar(),
     },
 
     offer : {
@@ -120,4 +34,3 @@ const creatAvailableApartment = function () {
 const temptArray = new Array (10).fill('').map(() => creatAvailableApartment());
 
 temptArray;
-
