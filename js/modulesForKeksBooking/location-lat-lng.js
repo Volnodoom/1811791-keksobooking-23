16425122610441a81980086028b6d5a249fa8869
Alexander <Bdiyoz@mail.ru>
2021-06-18 22:09:05 +0400

    Corrected Location-lat-lng (working)

--- js/modulesForKeksBooking/location-lat-lng.js
+++ js/modulesForKeksBooking/location-lat-lng.js
@@ -1,4 +1,4 @@
-import {getRandomPositiveFloat} from './get-random-positive-float.js';
+import {getRandomPositiveFloat} from './get-random.js';
 const LAT = getRandomPositiveFloat (35.65000, 35.70000,5);
 const LNG = getRandomPositiveFloat (139.70000,139.80000,5);
 
