dbf3ee6206a567bfc17193f967a421bc3ce64e30
Alexander <Bdiyoz@mail.ru>
2021-06-25 03:52:09 +0400

    Operational base for module7-task1 have been successfully complied

--- js/modules/generate-similar-advs.js
+++ js/modules/generate-similar-advs.js
@@ -24,20 +24,101 @@
 
 import {temptArray} from './generate-card.js';
 
+const randomData = temptArray;
 const advertizementTemplate = document.querySelector ('#card').content;
+
 const titleTemplate = advertizementTemplate.querySelector ('.popup__title');
 const addressTemplate = advertizementTemplate.querySelector ('.popup__text--address');
 const priseTemplate = advertizementTemplate.querySelector ('.popup__text--price');
 const apartmentTypeTemplate = advertizementTemplate.querySelector ('.popup__type');
-const roomsTemplate = advertizementTemplate.querySelector ('.popup__text--capacity');
+const capacityTemplate = advertizementTemplate.querySelector ('.popup__text--capacity');
 const timeTemplate = advertizementTemplate.querySelector ('.popup__text--time');
 const featuresTemplate = advertizementTemplate.querySelector ('.popup__features');
 const descriptionTemplate = advertizementTemplate.querySelector ('.popup__description');
 const photosTemplate = advertizementTemplate.querySelector ('.popup__photos');
 const avatarTemplate = advertizementTemplate.querySelector ('.popup__avatar');
 
+const boardForAdvz = document.querySelector ('#map-canvas');
+const similarListFragment = document.createDocumentFragment ();
+const addToFragment = function (partOfAdv){
+  similarListFragment.appendChild (partOfAdv);
+};
 
-console.log (avatarTemplate);
+randomData.forEach((advData) => {
 
+  const advTitle = titleTemplate.cloneNode (true);
+  advTitle.textContent = advData.offer.title;
+  addToFragment (advTitle);
+
+  const advAddress = addressTemplate.cloneNode (true);
+  advAddress.textContent = advData.offer.address;
+  addToFragment (advAddress);
+
+  const advPrice = priseTemplate.cloneNode (true);
+  advPrice.textContent = `${advData.offer.price} ₽/ночь`;
+  addToFragment (advPrice);
+
+  const advApartment = apartmentTypeTemplate.cloneNode (true);
+  //constanta =>
+  const comparisonEnglishWithRussian = {
+    flat: 'Квартира',
+    bungalow: 'Бунгало',
+    house: 'Дом',
+    palace: 'Дворец',
+    hotel: 'Отель',
+  };
+  const compareWithRightName = function (englishNameOfApart) {
+    return comparisonEnglishWithRussian[englishNameOfApart];
+  };
+  advApartment.textContent = compareWithRightName (advData.offer.type);
+  addToFragment(advApartment);
+
+  const advCapacity = capacityTemplate.cloneNode (true);
+  advCapacity.textContent = `${advData.offer.rooms} комнаты для ${advData.offer.guests} гостей`;
+  addToFragment(advCapacity);
+
+  const advTime = timeTemplate.cloneNode (true);
+  advTime.textContent = `Заезд после ${advData.offer.checkin}, выезд до ${advData.offer.checkout} гостей`;
+  addToFragment(advTime);
+
+  const advFeatures = featuresTemplate.cloneNode (true);
+  const advFeaturesClass = advData.offer.features.map((feature)=>
+    `popup__feature--${feature}`);
+  advFeatures.querySelectorAll ('.popup__feature')
+    .forEach ((feature) => {
+      const item = feature.classList[1];
+      if (!advFeaturesClass.includes(item)) {
+        feature.remove ();
+      }
+    });
+  addToFragment(advFeatures);
+
+  const advDescription = descriptionTemplate.cloneNode (true);
+  advDescription.textContent = advData.offer.description;
+  addToFragment (advDescription);
+
+  const advPhotos = photosTemplate.cloneNode (false);
+  const onePhotoTemplate = photosTemplate.querySelector('.popup__photo');
+  advData.offer.photos
+    .forEach ((photo)=> {
+      const advPhoto = onePhotoTemplate.cloneNode (true);
+      advPhoto.src = photo;
+      advPhotos.appendChild (advPhoto);
+    });
+  addToFragment (advPhotos);
+
+  const advAvatar = avatarTemplate.cloneNode (true);
+  advAvatar.src = advData.author.avatar;
+  addToFragment (advAvatar);
+  //console.log (advData.offer.photos);
+
+});
+
+
+boardForAdvz.appendChild (similarListFragment);
+
+//boardForAdvz.appendChild (advzTitle);
+
+console.log (boardForAdvz);
 
 
