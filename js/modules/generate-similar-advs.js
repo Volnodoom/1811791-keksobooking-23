29912075d63a2d3fd9add53e4a844345ec5f95e9
Alexander <Bdiyoz@mail.ru>
2021-06-25 19:16:41 +0400

    module7-task1 ready for supervisor check

--- js/modules/generate-similar-advs.js
+++ js/modules/generate-similar-advs.js
@@ -1,28 +1,16 @@
-/*<template id="card">
-    <article class="popup">
-      <img src="img/avatars/user01.png" class="popup__avatar" width="70" height="70" alt="Аватар пользователя">
-      <h3 class="popup__title">Уютное гнездышко для молодоженов</h3>
-      <p class="popup__text popup__text--address">102-0082 Tōkyō-to, Chiyoda-ku, Ichibanchō, 14−3</p>
-      <p class="popup__text popup__text--price">5200 <span>₽/ночь</span></p>
-      <h4 class="popup__type">Квартира</h4>
-      <p class="popup__text popup__text--capacity">2 комнаты для 3 гостей</p>
-      <p class="popup__text popup__text--time">Заезд после 14:00, выезд до 10:00</p>
-      <ul class="popup__features">
-        <li class="popup__feature popup__feature--wifi"></li>
-        <li class="popup__feature popup__feature--dishwasher"></li>
-        <li class="popup__feature popup__feature--parking"></li>
-        <li class="popup__feature popup__feature--washer"></li>
-        <li class="popup__feature popup__feature--elevator"></li>
-        <li class="popup__feature popup__feature--conditioner"></li>
-      </ul>
-      <p class="popup__description">Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.</p>
-      <div class="popup__photos">
-        <img src="" class="popup__photo" width="45" height="40" alt="Фотография жилья">
-      </div>
-    </article>
-  </template>*/
-
 import {temptArray} from './generate-card.js';
+
+const COMPARISON_ENGLISH_WITH_RUSSIAN = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+const conditionsEmpty = function (EmptyContent) {
+  return (EmptyContent === ' ') || (EmptyContent === undefined) || (EmptyContent === null);
+};
 
 const randomData = temptArray;
 const advertizementTemplate = document.querySelector ('#card').content;
@@ -38,69 +26,92 @@ const descriptionTemplate = advertizementTemplate.querySelector ('.popup__descri
 const photosTemplate = advertizementTemplate.querySelector ('.popup__photos');
 const avatarTemplate = advertizementTemplate.querySelector ('.popup__avatar');
 
-const boardForAdvz = document.querySelector ('#map-canvas');
+
+const boardForAdv = document.querySelector ('#map-canvas');
 const similarListFragment = document.createDocumentFragment ();
 const addToFragment = function (partOfAdv){
   similarListFragment.appendChild (partOfAdv);
+};
+
+const hideEmptyContent = function (EmptyContent, fieldToHide) {
+  if (conditionsEmpty (EmptyContent)) {
+    fieldToHide.classList.add ('hidden');}
 };
 
 randomData.forEach((advData) => {
 
   const advTitle = titleTemplate.cloneNode (true);
   advTitle.textContent = advData.offer.title;
+  hideEmptyContent (advTitle.textContent,advTitle);
   addToFragment (advTitle);
 
   const advAddress = addressTemplate.cloneNode (true);
   advAddress.textContent = advData.offer.address;
+  hideEmptyContent (advData.offer.address,advAddress);
   addToFragment (advAddress);
 
   const advPrice = priseTemplate.cloneNode (true);
   advPrice.textContent = `${advData.offer.price} ₽/ночь`;
+  hideEmptyContent (advData.offer.price, advPrice);
   addToFragment (advPrice);
 
   const advApartment = apartmentTypeTemplate.cloneNode (true);
-  //constanta =>
-  const comparisonEnglishWithRussian = {
-    flat: 'Квартира',
-    bungalow: 'Бунгало',
-    house: 'Дом',
-    palace: 'Дворец',
-    hotel: 'Отель',
-  };
   const compareWithRightName = function (englishNameOfApart) {
-    return comparisonEnglishWithRussian[englishNameOfApart];
+    return COMPARISON_ENGLISH_WITH_RUSSIAN [englishNameOfApart];
   };
   advApartment.textContent = compareWithRightName (advData.offer.type);
+  hideEmptyContent (advData.offer.type, advApartment);
   addToFragment(advApartment);
 
   const advCapacity = capacityTemplate.cloneNode (true);
-  advCapacity.textContent = `${advData.offer.rooms} комнаты для ${advData.offer.guests} гостей`;
+  if (conditionsEmpty (advData.offer.rooms)) {
+    advCapacity.textContent = `Для ${advData.offer.guests} гостей`;
+  } else if (conditionsEmpty (advData.offer.guests)) {
+    advCapacity.textContent =`${advData.offer.rooms} комнат`;
+  } else if ((conditionsEmpty (advData.offer.guests)) === (conditionsEmpty (advData.offer.rooms))) {
+    advCapacity.classList.add ('hidden');
+  }
+  else {
+    advCapacity.textContent =`${advData.offer.rooms} комнаты для ${advData.offer.guests} гостей`;
+  }
   addToFragment(advCapacity);
 
   const advTime = timeTemplate.cloneNode (true);
-  advTime.textContent = `Заезд после ${advData.offer.checkin}, выезд до ${advData.offer.checkout} гостей`;
+  if (conditionsEmpty(advData.offer.checkin)) {
+    advTime.textContent = `Выезд до ${advData.offer.checkout}`;
+  } else if (conditionsEmpty(advData.offer.checkout)) {
+    advTime.textContent = `Заезд после ${advData.offer.checkin}`;
+  } else if ((conditionsEmpty (advData.offer.checkout)) === (conditionsEmpty (advData.offer.checkin))){
+    advTime.classList.add ('hidden');
+  } else{
+    advTime.textContent = `Заезд после ${advData.offer.checkin}, выезд до ${advData.offer.checkout}`;
+  }
   addToFragment(advTime);
 
   const advFeatures = featuresTemplate.cloneNode (true);
-  const advFeaturesClass = advData.offer.features.map((feature)=>
-    `popup__feature--${feature}`);
-  advFeatures.querySelectorAll ('.popup__feature')
-    .forEach ((feature) => {
-      const item = feature.classList[1];
-      if (!advFeaturesClass.includes(item)) {
-        feature.remove ();
-      }
-    });
+  if(conditionsEmpty(advData.offer.features)){
+    advFeatures.classList.add ('hidden');
+  } else {
+    const advFeaturesClass = advData.offer.features.map((feature) =>
+      `popup__feature--${feature}`);
+    advFeatures.querySelectorAll ('.popup__feature')
+      .forEach ((feature) => {
+        const item = feature.classList[1];
+        if (!advFeaturesClass.includes(item)) {
+          feature.remove ();
+        }
+      });}
   addToFragment(advFeatures);
 
   const advDescription = descriptionTemplate.cloneNode (true);
   advDescription.textContent = advData.offer.description;
+  hideEmptyContent (advData.offer.description, advDescription);
   addToFragment (advDescription);
 
   const advPhotos = photosTemplate.cloneNode (false);
   const onePhotoTemplate = photosTemplate.querySelector('.popup__photo');
   advData.offer.photos
-    .forEach ((photo)=> {
+    .forEach ((photo) => {
       const advPhoto = onePhotoTemplate.cloneNode (true);
       advPhoto.src = photo;
       advPhotos.appendChild (advPhoto);
@@ -109,16 +120,9 @@ randomData.forEach((advData) => {
 
   const advAvatar = avatarTemplate.cloneNode (true);
   advAvatar.src = advData.author.avatar;
+  hideEmptyContent (advData.author.avatar, advAvatar);
   addToFragment (advAvatar);
-  //console.log (advData.offer.photos);
 
 });
 
-
-boardForAdvz.appendChild (similarListFragment);
-
-//boardForAdvz.appendChild (advzTitle);
-
-console.log (boardForAdvz);
-
-
+boardForAdv.appendChild (similarListFragment);
