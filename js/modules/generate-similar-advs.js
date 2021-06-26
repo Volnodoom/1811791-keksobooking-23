e69e5bfe222f094b28baa95495974293e3b809f0
Alexander <Bdiyoz@mail.ru>
2021-06-27 01:44:38 +0400

    Code corrected regarding first remarks of the Supervisor except of the field with Photos

--- js/modules/generate-similar-advs.js
+++ js/modules/generate-similar-advs.js
@@ -13,19 +13,20 @@ const conditionsEmpty = function (EmptyContent) {
 };
 
 const randomData = temptArray;
-const advertizementTemplate = document.querySelector ('#card').content;
+const advertisementTemplate = document.querySelector ('#card').content;
 
-const titleTemplate = advertizementTemplate.querySelector ('.popup__title');
-const addressTemplate = advertizementTemplate.querySelector ('.popup__text--address');
-const priseTemplate = advertizementTemplate.querySelector ('.popup__text--price');
-const apartmentTypeTemplate = advertizementTemplate.querySelector ('.popup__type');
-const capacityTemplate = advertizementTemplate.querySelector ('.popup__text--capacity');
-const timeTemplate = advertizementTemplate.querySelector ('.popup__text--time');
-const featuresTemplate = advertizementTemplate.querySelector ('.popup__features');
-const descriptionTemplate = advertizementTemplate.querySelector ('.popup__description');
-const photosTemplate = advertizementTemplate.querySelector ('.popup__photos');
-const avatarTemplate = advertizementTemplate.querySelector ('.popup__avatar');
-
+const titleTemplate = advertisementTemplate.querySelector ('.popup__title');
+const addressTemplate = advertisementTemplate.querySelector ('.popup__text--address');
+const priseTemplate = advertisementTemplate.querySelector ('.popup__text--price');
+const apartmentTypeTemplate = advertisementTemplate.querySelector ('.popup__type');
+const capacityTemplate = advertisementTemplate.querySelector ('.popup__text--capacity');
+const timeTemplate = advertisementTemplate.querySelector ('.popup__text--time');
+const featuresTemplate = advertisementTemplate.querySelector ('.popup__features');
+const descriptionTemplate = advertisementTemplate.querySelector ('.popup__description');
+const photosTemplate = advertisementTemplate.querySelector ('.popup__photos');
+const onePhotoTemplate = photosTemplate.querySelector('.popup__photo');
+const avatarTemplate = advertisementTemplate.querySelector ('.popup__avatar');
+const finalElementTemplate = advertisementTemplate.querySelector ('.popup');
 
 const boardForAdv = document.querySelector ('#map-canvas');
 const similarListFragment = document.createDocumentFragment ();
@@ -38,24 +39,35 @@ const hideEmptyContent = function (EmptyContent, fieldToHide) {
     fieldToHide.classList.add ('hidden');}
 };
 
-randomData.forEach((advData) => {
+const advTitle = titleTemplate.cloneNode (true);
+const advAddress = addressTemplate.cloneNode (true);
+const advPrice = priseTemplate.cloneNode (true);
+const advApartment = apartmentTypeTemplate.cloneNode (true);
+const advCapacity = capacityTemplate.cloneNode (true);
+const advTime = timeTemplate.cloneNode (true);
+const advFeatures = featuresTemplate.cloneNode (true);
+const advDescription = descriptionTemplate.cloneNode (true);
+const advPhotos = photosTemplate.cloneNode (false);
+const advAvatar = avatarTemplate.cloneNode (true);
+const advFinalElement = finalElementTemplate.cloneNode (false);
 
-  const advTitle = titleTemplate.cloneNode (true);
+randomData.forEach((advData) => {
+  advAvatar.src = advData.author.avatar;
+  hideEmptyContent (advData.author.avatar, advAvatar);
+  addToFragment (advAvatar);
+
   advTitle.textContent = advData.offer.title;
   hideEmptyContent (advTitle.textContent,advTitle);
   addToFragment (advTitle);
 
-  const advAddress = addressTemplate.cloneNode (true);
   advAddress.textContent = advData.offer.address;
   hideEmptyContent (advData.offer.address,advAddress);
   addToFragment (advAddress);
 
-  const advPrice = priseTemplate.cloneNode (true);
   advPrice.textContent = `${advData.offer.price} ₽/ночь`;
   hideEmptyContent (advData.offer.price, advPrice);
   addToFragment (advPrice);
 
-  const advApartment = apartmentTypeTemplate.cloneNode (true);
   const compareWithRightName = function (englishNameOfApart) {
     return COMPARISON_ENGLISH_WITH_RUSSIAN [englishNameOfApart];
   };
@@ -63,7 +75,6 @@ randomData.forEach((advData) => {
   hideEmptyContent (advData.offer.type, advApartment);
   addToFragment(advApartment);
 
-  const advCapacity = capacityTemplate.cloneNode (true);
   if (conditionsEmpty (advData.offer.rooms)) {
     advCapacity.textContent = `Для ${advData.offer.guests} гостей`;
   } else if (conditionsEmpty (advData.offer.guests)) {
@@ -76,7 +87,6 @@ randomData.forEach((advData) => {
   }
   addToFragment(advCapacity);
 
-  const advTime = timeTemplate.cloneNode (true);
   if (conditionsEmpty(advData.offer.checkin)) {
     advTime.textContent = `Выезд до ${advData.offer.checkout}`;
   } else if (conditionsEmpty(advData.offer.checkout)) {
@@ -88,7 +98,6 @@ randomData.forEach((advData) => {
   }
   addToFragment(advTime);
 
-  const advFeatures = featuresTemplate.cloneNode (true);
   if(conditionsEmpty(advData.offer.features)){
     advFeatures.classList.add ('hidden');
   } else {
@@ -103,26 +112,32 @@ randomData.forEach((advData) => {
       });}
   addToFragment(advFeatures);
 
-  const advDescription = descriptionTemplate.cloneNode (true);
   advDescription.textContent = advData.offer.description;
   hideEmptyContent (advData.offer.description, advDescription);
   addToFragment (advDescription);
 
-  const advPhotos = photosTemplate.cloneNode (false);
-  const onePhotoTemplate = photosTemplate.querySelector('.popup__photo');
-  advData.offer.photos
-    .forEach ((photo) => {
-      const advPhoto = onePhotoTemplate.cloneNode (true);
-      advPhoto.src = photo;
-      advPhotos.appendChild (advPhoto);
-    });
+
+  advData.offer.photos.forEach((photo) => {
+
+    const advPhoto = onePhotoTemplate.cloneNode (true);
+    advPhoto.src = photo;
+    advPhotos.appendChild (advPhoto);
+  });
   addToFragment (advPhotos);
-
-  const advAvatar = avatarTemplate.cloneNode (true);
-  advAvatar.src = advData.author.avatar;
-  hideEmptyContent (advData.author.avatar, advAvatar);
-  addToFragment (advAvatar);
-
 });
+advFinalElement.appendChild (similarListFragment);
+boardForAdv.appendChild (advFinalElement);
 
-boardForAdv.appendChild (similarListFragment);
+/*advData.offer.photos.forEach((photo) => {
+    {let ind = 0;
+      while ( ind <advData.offer.photo.length) {
+        if (ind === advData.offer.photo.length) {
+          break;
+        }
+        ind++;
+        const advPhoto = onePhotoTemplate.cloneNode (true);
+        advPhoto.src = photo;
+        advPhotos.appendChild (advPhoto);
+      }
+    }
+  });*/
